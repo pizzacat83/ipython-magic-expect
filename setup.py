d6063f7c6140d5fada03fa25a0df7967f3aa789c
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-05-07 19:11:09 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Expect',
+      version='0.1.0',
+      description='A IPython magic to assert the output of a cell',
+      author='pizzacat83',
+      author_email='pizzacat83.cpp@gmail.com',
+      url='https://github.com/pizzacat83/jupyter-magic-expect',
+      license='MIT',
+      packages=['expectmagic'],
+      install_requires=['ipython'],
+      keywords=['ipython', 'assert', 'test', 'jupyter', 'notebook']
+     )
