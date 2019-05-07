6843bf3fd4e614dab90576d77e411c3af40eb466
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-05-07 19:14:01 +0900

    fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='Expect',
       version='0.1.0',
-      description='A IPython magic to assert the output of a cell',
+      description='An IPython magic to assert the output of a cell',
       author='pizzacat83',
       author_email='pizzacat83.cpp@gmail.com',
       url='https://github.com/pizzacat83/jupyter-magic-expect',
